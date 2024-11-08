969c92140b99cc9840cbee092069beaf0e270ea4
R-kjim <rkayjim@gmail.com>
2024-11-08 14:26:57 +0300

    fixes bug

--- src/components/Homepage/HeroSection.jsx
+++ src/components/Homepage/HeroSection.jsx
@@ -1,6 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+import 'swiper/swiper-bundle.css';
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
