3ebbeb9b2cbf497f5dee7d05cc2a2a47c499b8a7
R-kjim <rkayjim@gmail.com>
2024-11-08 14:31:55 +0300

    fixes deployment bug

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['swiper']
-  }
+  },
+  build: {
+    rollupOptions: {
+      external: ['swiper/react'],
+    },
+  },
 })
