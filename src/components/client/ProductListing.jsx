8fcf03582b49e43bb94a2591955394d9ba83cd3a
R-kjim <rkayjim@gmail.com>
2024-11-13 08:43:36 +0300

    fixes design layout flaws-2

--- src/components/client/ProductListing.jsx
+++ src/components/client/ProductListing.jsx
@@ -176,7 +176,7 @@ const ProductListing = () => {
       {/* Buttons */}
       <div className="flex justify-between items-center mt-auto p-4">
         <button
-          className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          className="px-4 py-2 bg-gray-900 text-white font-semibold rounded-lg shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-opacity-50"
           onClick={() => navigate(`/client/product/${product.id}`)}
         >
           Details
